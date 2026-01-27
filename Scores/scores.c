ef91ba5072733a96030a1e40bb4f92b5dccf39fa
Charlie Bluu <charlesbluu@gmail.com>
2026-01-27 18:48:30 +0100

    Scores actually finished

--- Scores/scores.c
+++ Scores/scores.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
+#define MAX_SCORES 20
+
 int main(void){
     int n_scores;
     do{
         printf ("How many grades do you want to calculate the average of? ");
         scanf("%i", &n_scores);
-    } while(n_scores<0);
+    } while(n_scores<1 || n_scores > MAX_SCORES);
     
-    int scores[n_scores];
+    int scores[MAX_SCORES];
     for (int i = 0; i < n_scores; i++) {
         printf("Insert score %i: ", i + 1);
         scanf("%i", &scores[i]);
     }
-    int sum = 0
+    int sum = 0;
     for (int i = 0; i < n_scores; i++) {
         sum += scores[i];
     }
 
-    printf("The average of these scores is: %f\n", (float) sum / n_scores)`;
+    printf("The average of these scores is: %f\n", (float) sum / n_scores);
 
     return 0;
 }
